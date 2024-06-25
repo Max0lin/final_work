9b157920f69ee67bb99af0d084dbe86211273371
Max0lin <s11228223@cycu.edu.tw>
2024-06-25 15:02:08 +0800

    Update tool.c

--- tool.c
+++ tool.c
@@ -12,6 +12,46 @@ void printStudent(Student students[], int numStudents) {
          printf("%-15s %-12s %8d    %-10s\n", students[i].name, students[i].department, students[i].grade, students[i].sid);
     }
 }
+
+int readSRecordsFromCSV(const char* filename, SRecord srecords[], int maxSRecords) {
+    // 從CSV文件中讀取學生成績信息
+    FILE* file = fopen(filename, "r"); // r = read, w = write, a = append
+    if (!file) {
+        printf("Error: Unable to open file %s\n", filename);
+        return 0;
+    }
+    // Skip the header line
+    char line[1024];
+    fgets(line, 1024, file); 
+    int numSRecords = 0;
+    while (fgets(line, 1024, file) && numSRecords < maxSRecords) {
+        sscanf(line, "%[^,],%[^,],%d,%[^,],%lf,%lf,%lf,%lf,%lf", srecords[numSRecords].student.department, srecords[numSRecords].student.sid, &srecords[numSRecords].student.grade, srecords[numSRecords].student.name, &srecords[numSRecords].hw, &srecords[numSRecords].quiz, &srecords[numSRecords].midterm, &srecords[numSRecords].final, &srecords[numSRecords].other);
+        
+        srecords[numSRecords].total = srecords[numSRecords].hw * HW_WEIGHT + srecords[numSRecords].quiz * QUIZ_WEIGHT + srecords[numSRecords].midterm * MIDTERM_WEIGHT + srecords[numSRecords].final * FINAL_WEIGHT + srecords[numSRecords].other * OTHER_WEIGHT;
+        numSRecords++;
+
+    }
+    fclose(file);
+    return numSRecords;
+    
+
+}
+
+
+
+void printSRecords(SRecord srecords[], int numSRecords) {
+    // 打印學生成績信息
+    saveDefaultColor();
+    setColor(RED);
+    setBackgroundColor(YELLOW);
+    printf("%-10s %-14s %8s    %-8s %5s %5s %5s %5s %5s %5s\n", "Name", "Department", "Grade", "SID", "HW", "Quiz", "Midterm", "Final", "Other", "Total");
+    printf("-----------------------------------------------------------------------------\n");
+    resetColor();
+    for (int i = 0; i < numSRecords; i++) {
+        printf("%-15s %-12s %8d    %-10s %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f\n", srecords[i].student.name, srecords[i].student.department, srecords[i].student.grade, srecords[i].student.sid, srecords[i].hw, srecords[i].quiz, srecords[i].midterm, srecords[i].final, srecords[i].other, srecords[i].total);
+    }
+}
+
 void randominitStudents(Student students[], int numStudents)
 {
     // Seed the random number generator
