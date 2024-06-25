56e9545e36e73035f0c677bb2aef124fea0275cc
Max0lin <s11228223@cycu.edu.tw>
2024-06-25 15:01:40 +0800

    Update mysort.c

--- mysort.c
+++ mysort.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "myhead.h"
 #include <string.h>
+#include <stdio.h> // Add the header file for printf
 
 // Comparison function for qsort
 int compareStudents(const void* a, const void* b) {
@@ -18,12 +19,49 @@ int compareStudentsByName(const void* a, const void* b) {
 }
 
 
+// Comparison function for department for SRecord
+int compareSRecordsByDepartment(const void* a, const void* b) {
+    SRecord* srecordA = (SRecord*)a;
+    SRecord* srecordB = (SRecord*)b;
+    
+    return strcmp(srecordA->student.department, srecordB->student.department);
+}
+
+// Comparison function for total for SRecord
+int compareSRecordsByTotal(const void* a, const void* b) {
+    SRecord* srecordA = (SRecord*)a;
+    SRecord* srecordB = (SRecord*)b;
+    
+    return srecordA->total - srecordB->total;
+}
+
+// comparison function by SID for SRecord
+int compareSRecordsBySID(const void* a, const void* b) {
+    SRecord* srecordA = (SRecord*)a;
+    SRecord* srecordB = (SRecord*)b;
+    
+    return strcmp(srecordA->student.sid, srecordB->student.sid);
+}
+
 // Function to sort students by SID
 void sortStudents(Student* students, int numStudents, const char* sortBy) {
     if (strcmp(sortBy, "SID") == 0) {
         qsort(students, numStudents, sizeof(Student), compareStudents);
     } else if (strcmp(sortBy, "Name") == 0) {
         qsort(students, numStudents, sizeof(Student), compareStudentsByName);
+    } else {
+        // Handle invalid sortBy parameter
+        printf("Invalid sortBy parameter: %s\n", sortBy);
+    }
+}
+
+void sortSRecords(SRecord* srecords, int numSRecords, const char* sortBy) {
+    if (strcmp(sortBy, "Department") == 0) {
+        qsort(srecords, numSRecords, sizeof(SRecord), compareSRecordsByDepartment);
+    } else if (strcmp(sortBy, "Total") == 0) {
+        qsort(srecords, numSRecords, sizeof(SRecord), compareSRecordsByTotal);
+    } else if (strcmp(sortBy, "SID") == 0) {
+        qsort(srecords, numSRecords, sizeof(SRecord), compareSRecordsBySID);
     } else {
         // Handle invalid sortBy parameter
         printf("Invalid sortBy parameter: %s\n", sortBy);
